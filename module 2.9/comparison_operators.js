db1cfee3066a1667a7499b6359776ad91b30b2d4
Anton <odianboy@mail.ru>
2021-05-17 00:12:39 +0300

    module 2.9

---
+++ module 2.9/comparison_operators.js
@@ -0,0 +1,14 @@
+'use strict'
+
+// Каким будет результат этих выражений?
+
+/*
+5 > 4 Вывод: true
+"ананас" > "яблоко" Вывод: false
+"2" > "12" Вывод: true
+undefined == null Вывод: true
+undefined === null Вывод: false
+null == "\n0\n" Вывод: false
+null === +"\n0\n" Вывод: false
+
+ */
