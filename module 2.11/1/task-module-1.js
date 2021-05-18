e8da7b5dfc19f8c93833855674c05c6fd84a5db7
Anton <odianboy@mail.ru>
2021-05-18 23:32:04 +0300

    module 2.11

---
+++ module 2.11/1/task-module-1.js
@@ -0,0 +1,8 @@
+'use strict'
+
+/*
+Что выведет alert (ИЛИ)?
+Что выведет код ниже?
+ */
+
+alert( null || 2 || undefined ); // 2
---
+++ module 2.11/4/task-module-4.js
@@ -0,0 +1,8 @@
+'use strict'
+
+/*
+Что выведет alert (И)?
+Что выведет код ниже?
+ */
+
+alert( alert(1) && alert(2) ); // 1, а затем undefined. (изначально указал false)
