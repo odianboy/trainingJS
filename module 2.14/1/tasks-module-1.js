02c89df18b2276505161f527725f48f35319300f
Anton <odianboy@mail.ru>
2021-05-20 23:36:30 +0300

    fix git conflict and module 2.14

---
+++ module 2.14/1/tasks-module-1.js
@@ -0,0 +1,35 @@
+'use strict'
+
+/*
+Напишите "if", аналогичный "switch"
+Напишите if..else, соответствующий следующему switch:
+
+switch (browser) {
+  case 'Edge':
+    alert( "You've got the Edge!" );
+    break;
+
+  case 'Chrome':
+  case 'Firefox':
+  case 'Safari':
+  case 'Opera':
+    alert( 'Okay we support these browsers too' );
+    break;
+
+  default:
+    alert( 'We hope that this page looks ok!' );
+}
+ */
+
+let nameBrowser = prompt('Введите название браузера', '')
+
+if (nameBrowser === 'Edge') {
+    alert('Youve got the Edge!');
+} else if (nameBrowser === 'Chrome'
+    || nameBrowser === 'Firefox'
+    || nameBrowser === 'Safari'
+    || nameBrowser === 'Opera') {
+    alert('Okay we support these browsers too');
+} else {
+    alert('We hope that this page looks ok!');
+}
