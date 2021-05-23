86b3ffa8e4717dd10855c82ff1c9be5faeb286e5
Anton <odianboy@mail.ru>
2021-05-24 00:12:35 +0300

    module tasks (2.15, 2.17, 3.2, 3.5)

---
+++ module 2.15/3/tasks-module-3.js
@@ -0,0 +1,15 @@
+'use strict'
+
+/*
+Функция min(a, b)
+Напишите функцию min(a,b), которая возвращает меньшее из чисел a и b.
+ */
+
+function main(a, b) {
+
+    if (a < b) {
+        return a
+    } else {
+        return b
+    }
+}
